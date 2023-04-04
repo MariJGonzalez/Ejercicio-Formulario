8ed1414087762aabcf35a975a8fa61ea1ae148fd
MariJGonzalez <marijgonzalez@gmail.com>
2023-04-04 10:09:34 +0200

    Ejercicio-Formulario 4 abril 10:07
    Se ha cambiado el cuadro de observaciones para que se ejecute desde javascript.
    Lo he puesto de dos formas, al clicar sobre Mostrar, y que aparezca directamente.

---
+++ script.js
@@ -0,0 +1,15 @@
+
+/*funcion para escribir en html*/
+
+function observaciones()
+{
+
+    //la función permite escribir en html dentro de js, en una sola línea
+    // pongo \ delante de las comillas del 20 y el 30 para que no las detecte como final del write
+
+
+    document.write('<p><label>Observaciones:</label></p><p><textarea cols="20" rows="30"></textarea>></textarea></p>');
+
+    //document.write("<textarea cols="20" rows="30"></textarea>");
+
+}
